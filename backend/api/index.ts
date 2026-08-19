4382cf7607895dad07f5a9e6e5c53e29027a0c8b
yanskie856-code <yanskie856gmail.com>
2026-08-19 21:51:01 +0800

    Fix backend Vercel API routing

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,16 +1,4 @@
-import path from 'path';
-
-let app: any;
-
-try {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const mainApp = require(path.join(__dirname, '../src/index'));
-  app = mainApp.default || mainApp;
-} catch (error) {
-  console.error('Failed to load main app:', error);
-  throw error;
-}
+import app from '../src/index';
 
 export default app;
-
 
